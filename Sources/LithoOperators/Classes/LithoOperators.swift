d94e411292520ba65e4c003ec5efc56c27bb3cc1
Calvin Collins <cjc8@williams.edu>
2021-06-22 11:47:03 -0700

    added new try cases

--- Sources/LithoOperators/Classes/LithoOperators.swift
+++ Sources/LithoOperators/Classes/LithoOperators.swift
@@ -377,6 +377,32 @@ public func tryCatch<T, U, V>(f: @escaping (T, U) throws -> V, catcher: @escapin
     }
 }
 
+public func tryCatch<T, U, V>(f: @escaping (T) throws -> U, catcher: @escaping (V) -> Void) -> (T) -> U? where V: Error {
+    return { t in
+        do {
+            return try f(t)
+        } catch {
+            if let err = error as? V {
+                catcher(err)
+            }
+            return nil
+        }
+    }
+}
+
+public func tryCatch<T, U, V, X>(f: @escaping (T, U) throws -> V, catcher: @escaping (X) -> Void) -> (T, U) -> V? where X: Error {
+    return { t, u in
+        do {
+            return try f(t, u)
+        } catch {
+            if let err = error as? X {
+                catcher(err)
+            }
+            return nil
+        }
+    }
+}
+
 //higher order functions
 
 /**
